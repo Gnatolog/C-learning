d1fc9c3935415cd6872587fc83f5b8514cd0d6d3
Robert <dr.ararturovich@gmail.com>
2023-04-09 14:20:23 +0300

    Max

--- number9/Program.cs
+++ number9/Program.cs
@@ -1,23 +1,25 @@
-﻿int a1 = 15;
+﻿int Max (int arg1,int arg2, int arg3)
+{
+       int result = arg1;
+       if (arg2 > result) result = arg2;
+       if (arg3 > result) result = arg3;
+       return result;
+
+}
+
+int a1 = 15;
 int b1 = 34;
 int c1 = 45555;
 int a2 = 158;
 int b2 = 344;
 int c2 = 425;
-int a3 = 135;
+int a3 = 1355565;
 int b3 = 234;
 int c3 = 345;
 
-int max = a1;
-if (b1>max) max = b1;
-if (c1>max) max = c1;
-
-if (a2>max) max = a2;
-if (b2>max) max = b2;
-if (c2>max) max = c2;
-
-if (a3>max) max = a3;
-if (b3>max) max = b3;
-if (c3>max) max = c3;
+// int max1 = Max(a1,b1,c1);
+// int max2 = Max(a2,b2,c2);
+// int max3 = Max(a3,b3,c3);
+int max = Max(Max(a1,b1,c1),Max(a2,b2,c2),Max(a3,b3,c3));
 
 Console.WriteLine("Maximum value = " + max);
